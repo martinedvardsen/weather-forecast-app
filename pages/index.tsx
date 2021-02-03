bd1588a073fa7922906b3d4052d841f743911b98
Martin Edvardsen <med@it-minds.dk>
2021-02-03 22:46:47 +0100

    testing process env url

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,7 +4,7 @@ import Weather from '../components/weather';
 
 export const getServerSideProps: GetServerSideProps = async (context) => { 
   const { city } = context.query;
-  const res = await fetch(`https://weather-forecast-app-6n83gb5ru.vercel.app/api/weatherAPI?city=${city || 'København'}`)
+  const res = await fetch(`${process.env.NOW_URL}/api/weatherAPI?city=${city || 'København'}`)
   const data = await res.json();
 
   return {
